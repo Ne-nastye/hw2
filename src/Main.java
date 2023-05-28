e04a0fd151bdb7402fc6147a9645185730dbb258
Nastasya <Nenastye90@gmail.com>
2023-05-28 13:45:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 25000; // стоимость билета
+        int y = 20; // кол-во руб для 1 бонусной мили
+        int n = x / y; // кол-во бонусных миль
+
+            System.out.println(n);
+        }
+    }
